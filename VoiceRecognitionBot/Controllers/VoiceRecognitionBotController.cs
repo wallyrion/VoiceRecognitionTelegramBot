a00e60766d521f55eb0266a818513140ecd97910
Alexey Kornienko <alexey.kornienko@smartodds.co.uk>
2022-01-27 21:19:42 +0200

    hello there

--- VoiceRecognitionBot/Controllers/VoiceRecognitionBotController.cs
+++ VoiceRecognitionBot/Controllers/VoiceRecognitionBotController.cs
@@ -24,7 +24,7 @@ namespace VoiceRecognitionBot.Controllers
         {
             _logger.LogError("Some error occured {0}", DateTime.UtcNow);
             _logger.LogInformation("Status called");
-            return Ok("new healthcheck" + DateTime.UtcNow.ToShortDateString());
+            return Ok("Hello there ! " + DateTime.UtcNow.ToShortDateString());
         }
 
         [HttpGet("Subscribe")]
